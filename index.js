16f053656b0c65ae382d4f738643bb6ae5e392b0
alanheredero11 <alanheredero11@gmail.com>
2022-10-20 15:32:36 +0200

    Upload Rama2

---
+++ index.js
@@ -0,0 +1,25 @@
+function suma (op1, op2, op3){
+    
+    let resultado = op1 + op2 + op3;
+    return resultado; 
+}
+
+function resta (op1, op2){
+    
+    let resultado = op1 - op2;
+    return resultado; 
+}
+
+function multiplicacion (op1, op2){
+    
+    let resultado = op1 * op2;
+    return resultado; 
+}
+
+function division (op1, op2){
+    
+    let resultado = op1 / op2;
+    return resultado; 
+}
+
+module.exports = {suma, resta, multiplicacion, division}
---
+++ run.js
@@ -0,0 +1,9 @@
+let my_lib = require("./index.js");
+
+console.log(my_lib.suma(4, 7))
+
+console.log(my_lib.resta(4, 7))
+
+console.log(my_lib.multiplicacion(4, 7))
+
+console.log(my_lib.division(4, 7))
